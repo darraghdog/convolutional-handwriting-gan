ba5cd7d9af220f3b12d41bd55c73704665e80ac1
Darragh Hanley <darragh.hanley@gmail.com>
2021-01-05 17:15:00 +0000

    Generate words manually

---
+++ generate_words.py
@@ -0,0 +1,59 @@
+from PIL import Image
+import torch
+import numpy as np
+from options.test_options import TestOptions
+from models.BigGAN_networks import Generator
+import data.alphabets as alphabets
+from util.util import prepare_z_y
+
+
+def load_model():
+    opt = TestOptions().parse()  # get test options
+    opt.n_classes = 80
+    gen = Generator(**vars(opt))
+
+    state_dict = torch.load('./checkpoints/140_net_G.pth')
+    if hasattr(state_dict, '_metadata'):
+        del state_dict._metadata
+    gen.load_state_dict(state_dict)
+    gen.eval()
+    return gen
+
+model = load_model()
+
+char_to_int = dict((s,t) for t,s in enumerate(alphabets.alphabetEnglish))
+
+def get_word(word):
+    encoded = [char_to_int[char] for char in word]
+    words = torch.zeros((1, len(encoded), 80), dtype=torch.int32)
+    for i, code in enumerate(encoded):
+        words[0, i, code] = 1
+    return words
+
+def generate_image(word, seed = None):
+    if seed is None:
+        seed = np.random.randint(0, 10e4)
+    words = get_word(word)
+    z, _ = prepare_z_y(1, 128, 80, device='cpu', seed=seed)
+    res = model.forward(z=z, y=words)
+    res = res.detach().numpy()[0, 0] * 255
+    im = Image.fromarray(res).convert('RGB')
+    return im
+
+# SHIBUYA SOLASTA 14F, 1-21-1 Dogenzaka, Shibuya, Tokyo, 150-0043 Japan
+img = generate_image('Here we go, Tatsuya')
+img
+
+SEED = 10001
+addr = 'SHIBUYA SOLASTA 14F, 1-21-1 Dogenzaka, Shibuya, Tokyo, 150-0043 Japan'
+ls = addr.split(', ')
+#ls = ['1-21-1 Dogenzaka,']*10
+buffer = np.ones((32, 10, 3), dtype = np.uint8)*255
+[ [ii for ii in i.split()] for i in ls]
+out = [[np.concatenate((np.array(generate_image(ii, seed = SEED)), buffer), 1) for ii in i.split()] for i in ls]
+out = [np.concatenate(w, 1) for w in out   ]
+max_w = max([i.shape[1] for i in out])
+
+out = [ np.concatenate((i, np.ones((32, 10 + max_w - i.shape[1],3), dtype = np.uint8)*255),1) for i in out]
+Image.fromarray(np.concatenate(out, 0))
+
